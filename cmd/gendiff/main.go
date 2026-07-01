316c78ca08ec026ade2c216a44b16f7f1d26ed40
Konstantin Korovin <cosmo.stan@yandex.com>
2026-07-01 19:51:57 +0300

    feat: implement CLI interface for gendiff

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func run(args []string, stdout, stderr io.Writer) int {
+	cmd := &cli.Command{
+		Name:  "gendiff",
+		Usage: "Compares two configuration files and shows a difference.",
+		Action: func(ctx context.Context, c *cli.Command) error {
+			fmt.Fprint(stdout, "I can't do anything yet, but I will be able to soon!\n")
+			fmt.Fprint(stdout, "To display help, use the --help flag\n")
+			return nil
+		},
+	}
+
+	err := cmd.Run(context.Background(), args)
+	if err != nil {
+		fmt.Fprintf(stderr, "Error: %s\n", err.Error())
+		return 1
+	}
+
+	return 0
+}
 
 func main() {
-	fmt.Println("Hello from a new Go repository")
+	os.Exit(run(os.Args, os.Stdout, os.Stderr))
 }
