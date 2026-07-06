9a9642ca95f3ff95ec40772327b471b2c0d427e0
Konstantin Korovin <cosmo.stan@yandex.com>
2026-07-06 18:25:45 +0300

    test: add test for YAML stylish output and remove unnecessary test cases

--- gendiff_test.go
+++ gendiff_test.go
@@ -19,25 +19,8 @@ func TestGendiff_Stylish(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, want, got)
-}
 
-func TestGenDiff_UnsupportedExt(t *testing.T) {
-	_, err := GenDiff("testdata/file1.json", "testdata/unsupported.txt", "stylish")
-	assert.Error(t, err)
-
-	_, err = GenDiff("testdata/unsupported.txt", "testdata/file2.json", "stylish")
-	assert.Error(t, err)
-}
-
-func TestGenDiff_NonExistentFile(t *testing.T) {
-	_, err := GenDiff("unknown.json", "testdata/file2.json", "stylish")
-	assert.Error(t, err)
-
-	_, err = GenDiff("testdata/file1.json", "unknown.json", "stylish")
-	assert.Error(t, err)
-}
-
-func TestGendiff_ParseError(t *testing.T) {
-	_, err := GenDiff("testdata/broken.json", "testdata/file2.json", "stylish")
-	assert.Error(t, err)
+	got, err = GenDiff("testdata/file1.yml", "testdata/file2.yaml", "stylish")
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
 }
