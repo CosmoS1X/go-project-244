2377999c8c0f61b99c3a5baa4887f2e1a1cff2d3
Konstantin Korovin <cosmo.stan@yandex.com>
2026-07-08 22:18:25 +0300

    test: rewrite tests for parsers using in-memory files

--- parsers/parsers_test.go
+++ parsers/parsers_test.go
@@ -1,67 +1,99 @@
 package parsers
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseFile(t *testing.T) {
-	cases := []struct {
-		name    string
-		path    string
-		want    ParsedData
-		wantErr bool
-	}{
-		{
-			name: "valid JSON",
-			path: "../testdata/file1.json",
-			want: ParsedData{
-				"host":    "hexlet.io",
-				"timeout": float64(50), // JSON numbers are unmarshaled as float64
-				"proxy":   "123.234.53.22",
-				"follow":  false,
+func TestParseFile_JSON(t *testing.T) {
+	dir := t.TempDir()
+	path := filepath.Join(dir, "config.json")
+	json := `{"group1":{"baz":"bas","foo":"bar","nest":{"key": "value"}}}`
+
+	err := os.WriteFile(path, []byte(json), 0o600)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := ParsedData{
+		"group1": ParsedData{
+			"baz": "bas",
+			"foo": "bar",
+			"nest": ParsedData{
+				"key": "value",
 			},
-		},
-		{
-			name: "valid YAML",
-			path: "../testdata/file2.yaml",
-			want: ParsedData{
-				"timeout": int(20), // YAML numbers are unmarshaled as int
-				"verbose": true,
-				"host":    "hexlet.io",
-			},
-		},
-		{
-			name:    "broken file",
-			path:    "../testdata/broken.json",
-			wantErr: true,
-		},
-		{
-			name:    "non-existent path",
-			path:    "../testdata/unknown.json",
-			wantErr: true,
-		},
-		{
-			name:    "unsupported file extension",
-			path:    "../testdata/unsupported.txt",
-			wantErr: true,
 		},
 	}
 
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			t.Parallel()
+	got, err := ParseFile(path)
 
-			got, err := ParseFile(c.path)
-			if c.wantErr {
-				assert.Error(t, err)
-				assert.Empty(t, got)
-				return
-			}
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
 
-			assert.NoError(t, err)
-			assert.Equal(t, c.want, got)
-		})
+func TestParseFile_YAML(t *testing.T) {
+	dir := t.TempDir()
+	path := filepath.Join(dir, "config.yaml")
+	yaml := "group2:\n  abc: 12345\n  deep:\n    id: 45\n"
+
+	err := os.WriteFile(path, []byte(yaml), 0o600)
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	want := ParsedData{
+		"group2": ParsedData{
+			"abc": 12345,
+			"deep": ParsedData{
+				"id": 45,
+			},
+		},
+	}
+
+	got, err := ParseFile(path)
+
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
+
+func TestParseFile_BrokenFile(t *testing.T) {
+	dir := t.TempDir()
+	path := filepath.Join(dir, "broken.json")
+	brokenJSON := `{"group1":{"baz":"bas","foo":"bar","nest":{"key": "value"}}` // Missing closing brace
+
+	err := os.WriteFile(path, []byte(brokenJSON), 0o600)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := ParseFile(path)
+
+	assert.Error(t, err)
+	assert.Empty(t, got)
+}
+
+func TestParseFile_NonExistentPath(t *testing.T) {
+	path := "nonexistent.json"
+
+	got, err := ParseFile(path)
+	assert.Error(t, err)
+	assert.Empty(t, got)
+}
+
+func TestParseFile_UnsupportedExtension(t *testing.T) {
+	dir := t.TempDir()
+	path := filepath.Join(dir, "config.txt")
+
+	err := os.WriteFile(path, []byte("some text"), 0o600)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := ParseFile(path)
+
+	assert.Error(t, err)
+	assert.Empty(t, got)
 }
