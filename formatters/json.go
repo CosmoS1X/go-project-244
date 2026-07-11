1f51354bd9c5a6644086c856e42847738308be0b
Konstantin Korovin <cosmo.stan@yandex.com>
2026-07-11 19:26:43 +0300

    fix: wrap JSON output in a root node and set 2-space indent

--- formatters/json.go
+++ formatters/json.go
@@ -9,11 +9,23 @@ import (
 
 type jsonFormatter struct{}
 
-func (j *jsonFormatter) Format(diffNodes []diff.Diff) string {
-	indentSym := " "
-	indentSize := 4
+type rootNode struct {
+	Key      string      `json:"key"`
+	Type     string      `json:"type"`
+	Children []diff.Diff `json:"children"`
+}
 
-	data, _ := json.MarshalIndent(diffNodes, "", strings.Repeat(indentSym, indentSize))
+func (j *jsonFormatter) Format(diffNodes []diff.Diff) string {
+	root := rootNode{
+		Key:      "",
+		Type:     "root",
+		Children: diffNodes,
+	}
+
+	indentSym := " "
+	indentSize := 2
+
+	data, _ := json.MarshalIndent(root, "", strings.Repeat(indentSym, indentSize))
 
 	return string(data)
 }
